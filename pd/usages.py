4d86b490ff311e125c4ef436df334bb28744d29f
join <xgzhangbing@126.com>
2018-05-10 10:06:05 +0800

    增加pandas的常见使用方法

---
+++ pd/usages.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+
+# 按...分组，按...排序
+df = pd.DataFrame()
+def valsort(row):
+    row['absvalue'] = row['value'].abs()
+    return row
+gdf = df.groupby(['item', 'itemID']).apply(valsort).sort(['item', 'absvalue'],
+                                                    ascending=[1,0]).reset_index()
