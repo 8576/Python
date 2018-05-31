ad3a11ce1bef435730835dc6da89d6b286e0142f
join <xgzhangbing@126.com>
2018-05-31 20:07:08 +0800

    添加matplotlib操作

---
+++ matplotlib/demone.py
@@ -0,0 +1,4 @@
+#matplotlib 使用说明
+
+#指定x坐标
+plt.xticks(np.arange(1,11),top10.index)
--- pd/usages.py
+++ pd/usages.py
@@ -7,5 +7,6 @@ df = pd.DataFrame()
 def valsort(row):
     row['absvalue'] = row['value'].abs()
     return row
+
 gdf = df.groupby(['item', 'itemID']).apply(valsort).sort(['item', 'absvalue'],
                                                     ascending=[1,0]).reset_index()
